2ef3612bfb1db8eb6424485a6a24d998ab9acf16
AliServat <aliservat20@gmail.com>
2021-11-07 10:10:47 +0330

    work is at a pleasing state now... only thing left to do is maybe add some more features

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+//GLOBAL VARIABLES
+
 const input = document.querySelector('input');
 const checkBtn = document.querySelector('.input-div button');
 const lettersDiv = document.querySelector('.letters-div');
 const mistakes = document.querySelector('.mistakes');
 const results = document.querySelector('.results');
+const resultpara = document.querySelector('.result');
+const error = document.querySelector('.error');
+let word;
+let answer;
 
-let word = 'hangman';
-word = word.split('');
-createSpace(word);
+creteWord();
 let wrongAnswers = [];
 
 input.focus();
 
-
 //EVENT LISTENERS
 
 checkBtn.addEventListener('click',(e)=>{
-    const indexes = check(word,input.value);
+    const regex = /[a-z]/i;
+    const test = regex.test(input.value.toLowerCase());
+    console.log(test);
+    if(test === true){
+    const indexes = check(word,input.value.toLowerCase());
     placeText(indexes,'.letters-div div');
     const divs = document.querySelectorAll('.mistakes div');
     for(i=0;i<wrongAnswers.length;i++){
@@ -24,10 +31,41 @@ checkBtn.addEventListener('click',(e)=>{
     input.value = '';
     gameResult();
     input.focus();
+    } else{
+        showError();
+    }
+})
+
+input.addEventListener('keyup',(e)=>{
+    if(e.key === 'Enter'){
+    const regex = /[a-z]/;
+    const test = regex.test(input.value.toLowerCase());
+    if(test === true){
+    const indexes = check(word,input.value.toLowerCase());
+    placeText(indexes,'.letters-div div');
+    const divs = document.querySelectorAll('.mistakes div');
+    for(i=0;i<wrongAnswers.length;i++){
+    divs[i].innerHTML = wrongAnswers[i];
+    }
+    input.value = '';
+    gameResult();
+    input.focus();
+    } else{
+        showError();
+    }
+    }
 })
 
 
 // FUNCTIONS
+
+function creteWord(){
+    word = countryList[Math.floor(Math.random()*(countryList.length+1))];
+    answer = word;
+    word = word.toLowerCase();
+    word = word.split('');
+    createSpace(word);
+}
 
 function createSpace(word){
     const length = word.length;
@@ -62,14 +100,13 @@ function check(array,inputValue){
 function placeText(array,targetDiv){
     const subspaces = document.querySelectorAll(targetDiv);
     array.forEach(index=>{
-        subspaces[index].innerText = input.value;
+        subspaces[index].innerText = input.value.toLowerCase();
     })
 }
 
 function gameResult(){
     const correct = document.querySelectorAll('.letters-div div');
     const wrong = document.querySelectorAll('.mistakes div');
-    const resultpara = document.querySelector('.result');
 
     let corrects = [];
     let wrongs = [];
@@ -80,6 +117,8 @@ function gameResult(){
 })
     if(corrects.length === word.length){
         resultpara.innerHTML = 'VICTORY!';
+        resultpara.style.color = 'green';
+        resultpara.style.textShadow = 'black 1px 1px';
         restartGame();
     }
 
@@ -90,12 +129,18 @@ function gameResult(){
 })
     if(wrongs.length === word.length){
         resultpara.innerHTML = 'DEFEAT!';
+        resultpara.style.color = 'red';
+        resultpara.style.textShadow = 'black 1px 1px';
+        const correctAnswer = document.createElement('p');
+        correctAnswer.innerHTML = 'Answer: ' + answer;
+        resultpara.appendChild(correctAnswer);
         restartGame();
     }
 }
 
 function restartGame(){
     checkBtn.disabled = true;
+    input.disabled = true;
     const restartBtn = document.createElement('button');
     restartBtn.innerHTML = 'Restart';
     results.appendChild(restartBtn);
@@ -115,9 +160,25 @@ function restartGame(){
         wrongAnswers = [];
 
         checkBtn.disabled = false;
+        input.disabled = false;
+        resultpara.innerHTML = '';
         results.removeChild(restartBtn);
+        lettersDiv.innerHTML = '';
+        mistakes.innerHTML = '';
+        creteWord();
         input.focus();
     })
+}
+
+function showError(){
+    error.style.padding = '10px';
+    error.innerHTML = 'Please Enter a valid value!';
+    input.value = '';
+    input.focus();
+    setTimeout(function(){
+        error.style.padding = '0px';
+        error.innerHTML = '';
+    },3000);
 }
 
 
